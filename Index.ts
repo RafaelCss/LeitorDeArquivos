2f45288e146b563a4190a4548a89f24a8afdceb4
Rafael Carlos De Souza <rf2014souza@gmail.com>
2022-01-06 12:05:29 -0300

    Atualizando

--- Index.ts
+++ Index.ts
@@ -19,42 +19,27 @@ function lerArquivos(dir: string) { // Ler arquivos de um diretório e salvar em
   }
 } */
 
-
-
 lerArquivosXls("./src");
 
-async function lerArquivosXls(dir: string) { //ler o Csv e salvar em xml 
+async function lerArquivosXls(dir: string) {
+  //ler o Csv e salvar em xml
 
   const arquivosLidos = fs.readFileSync(`${dir}/${"teste.xls"}`, "utf-8");
-  const texto = Array.from(arquivosLidos.split(" "));
-  
-
- const array = [];
-  for (let i in texto) {
-    array.push(texto[i]);
+  const texto = arquivosLidos.split(" ");
+  var array = [];
+  for (let i = 0; i < texto.length; i++) {
+    array.push(texto);
   }
 
-   Array.from(array).forEach(element => {
-    console.log(element);
-    });
+  const textoDoArray = array.toString();
 
+  console.log(textoDoArray)
 
-
-// função para  salvar cada palabvra em um arquivo xml
-
-
-
-
-
-
-  const teste = fs.writeFile("teste.xls", arquivosLidos, function (err) {
+  const teste = fs.writeFile("teste.xls", textoDoArray, function (err) {
     if (!err) {
       console.log("Arquivo salvo com sucesso!");
     } else {
       console.log(err);
     }
   });
-
-
-
 }
