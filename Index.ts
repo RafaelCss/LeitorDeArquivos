c0e412d57a9ff066fe2fa9876e4377fc0d109992
Rafael Carlos De Souza <rf2014souza@gmail.com>
2022-01-11 23:46:44 -0300

    :package: Atualização

--- Index.ts
+++ Index.ts
@@ -1,44 +1,21 @@
 import fs from "fs";
 
-/* lerArquivos("./src");
+lerArquivos("./src");
 
 
-function lerArquivos(dir: string) { // Ler arquivos de um diretório e salvar em csv
+async function lerArquivos(dir: string) { // Ler arquivos de um diretório e salvar em csv
     
-  const lerArquivosDaPasta = fs.readdirSync(dir);
-
-  for (let i in lerArquivosDaPasta) {
-    const arquivosLidos = fs.readFileSync(`${dir}/${lerArquivosDaPasta[i]}`, "utf-8");
-    const teste = fs.writeFile("teste.xls", arquivosLidos,function(err){
-        if(!err){
-            console.log("Arquivo salvo com sucesso!")
-        }else{
-        console.log(err)
-        } 
-    });
-  }
-} */
-
-lerArquivosXls("./src");
-
-async function lerArquivosXls(dir: string) {
-  //ler o Csv e salvar em xml
-
-  const arquivosLidos = fs.readFileSync(`${dir}/${"teste.xls"}`, "utf-8");
-  const texto = arquivosLidos.split(" ");
-  var array = [];
-  for (let i = 0; i < texto.length; i++) {
-    array.push(texto);
-  }
-
-  const textoDoArray = array.toString();
-  const limpezaTexto = textoDoArray.replace(" ","Um" )
-
-  const teste = fs.writeFile("teste.xls", limpezaTexto, function (err) {
-    if (!err) {
-      console.log("Arquivo salvo com sucesso!");
-    } else {
-      console.log(err);
+   const lerArquivosDaPasta =  fs.readdirSync(dir);
+    var arquivo="";
+    for(let lerArquivos of lerArquivosDaPasta) {
+        arquivo+=fs.readFileSync(`${dir}/${lerArquivos}`, "utf-8")
     }
-  });
-}
+
+ const teste = fs.writeFile("teste.xls", arquivo.toString(),function(err){
+    if(!err){
+        console.log("Arquivo salvo com sucesso!")
+    }else{
+    console.log(err)
+    } 
+  }); 
+} 
